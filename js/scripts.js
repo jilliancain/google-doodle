cf53e050fd51e34ffc04fb7abd12d4a5413f3dd1
jilliancain <jdocherty@vcd-ch204-02.eastern.ewu.edu>
2017-02-22 13:45:04 -0800

    randomize image
    *random homepage images
    *javascript to randomize, not scss

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-// YOUR SCRIPTS GO HERE
+// store 3 images
+
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+
+// get random #
+
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+
+//display random images
+
+$('img').attr('src', eval('img' + numRand) );
